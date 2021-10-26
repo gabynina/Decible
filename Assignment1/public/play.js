c35f1916d11f14ac6a810066490d46d6f240bbac
kyliewasserman <79178779+kyliewasserman@users.noreply.github.com>
2021-10-26 15:11:52 -0400

    2nd post statement done

---
+++ Assignment1/public/play.js
@@ -0,0 +1,65 @@
+const submitSongFunction = function( k ) {
+    console.log('In submitSong')
+    k.preventDefault()
+    
+    const a = document.querySelector( '#artist' ),
+          t = document.querySelector( '#title' ),
+          json = { artist: a.value, title: t.value},
+          body = JSON.stringify( json )
+    
+    fetch( '/submit-message', {
+      method:'POST',
+      body 
+    })
+    .then( function( response ) {
+      return response.json()
+    })
+    .then( function( json ) {
+      console.log(json)
+    
+      reload(json)
+      document.querySelector("#enter").reset();
+    })
+    
+    return false
+  }
+  
+  const reload = (json) => {
+    var newH33 = document.createElement('h3')
+      var h3Text2 = '<h3 id="newText2">'
+      h3Text2 +='<p>You have submitted '
+      h3Text2 += json[json.length-1].title
+      h3Text2 += ' by '
+      h3Text2 += json[json.length-1].artist
+      h3Text2 += '.</p>'
+      h3Text2 += '</h3>'
+      newH33.innerHTML = h3Text2
+      if(document.querySelector('#newText2') != null){
+        document.querySelector('#newText2').remove()
+      }
+      // document.querySelector('#newText2').remove()
+      var trythis = document.querySelector('#thislocation')
+      trythis.appendChild(newH33)
+  }
+    
+  
+  function handleSubmit(event) {
+    console.log('In reload5')
+    event.preventDefault();
+  
+    const data = new FormData(event.target);
+  
+    const value = Object.fromEntries(data.entries());
+  
+    value.topics = data.getAll("topics");
+  
+    console.log({ value });
+  }
+  
+    
+    window.onload = function(){
+      console.log('window.onload 1')
+      const submitSong = document.querySelector('#submitSong2')
+      console.log('window.onload 2')
+      submitSong.onmousedown = submitSongFunction
+    }
--- Assignment1/public/script.js
+++ Assignment1/public/script.js
@@ -27,18 +27,8 @@ const submitMessageFunction = function( e ) {
 }
 
 const reload = (json) => {
-  console.log('In reload')
   var newH3 = document.createElement('h3')
     var h3Text = '<h3 id="newText">'
-    // var i = 0
-    //   while (json[i]!=undefined) {
-    //     h3Text +='<p>Hi, '
-    //     h3Text += json[i].name
-    //     h3Text += ' we have recieved your message and will get back to you at '
-    //     h3Text += json[i].email
-    //     h3Text += '.</p>'
-    //   i++
-    // }
     h3Text +='<p>Hi, '
     h3Text += json[json.length-1].name
     h3Text += ' we have recieved your message and will get back to you at '
@@ -55,6 +45,7 @@ const reload = (json) => {
   
 
 function handleSubmit(event) {
+  console.log("handleing submit")
   event.preventDefault();
 
   const data = new FormData(event.target);
@@ -68,13 +59,10 @@ function handleSubmit(event) {
 
   
   window.onload = function(){
-    //const submitMessage = document.querySelector('#submit')
-    //submitMessage.addEventListener("submit", function()) = submit
     console.log('window.onload 1')
     const submitMessage = document.querySelector('#submit')
     console.log('window.onload 2')
     submitMessage.onmousedown = submitMessageFunction
-    console.log('window.onload 3')
-    // submitMessage.addEventListener("click", submitMessageFunction)
-    console.log('window.onload 4')
   }
+
+
